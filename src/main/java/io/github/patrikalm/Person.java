c6381b8905bfacc45256076dd028df55cfe0f14a
Patrik Alm <almpatrik@yahoo.se>
2024-10-28 19:10:31 +0100

    Started to add code to class Person and tried to update the pom with JUnit configuration.

--- src/main/java/io/github/patrikalm/Person.java
+++ src/main/java/io/github/patrikalm/Person.java
@@ -2,5 +2,35 @@ package io.github.patrikalm;
 
 public class Person {
 
+    private int id;
+    private String firstName;   //have to fix check null not allowed
+    private String lastName;    //have to fix check null not allowed
+    private String email;
+
+
+
+    //constructor
+
+
+    //methods
+
+    public int getId() {
+
+        //have to make a sequencer
+
+        return id;
+    }
+
+    public String getFirstName() {
+
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+
+        this.firstName = firstName;
+    }
+
+
 
 }
