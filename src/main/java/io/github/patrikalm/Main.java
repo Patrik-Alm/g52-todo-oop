20bd6c0f4cf74070ad193c53eca85051dd02b6c3
Patrik Alm <almpatrik@yahoo.se>
2024-10-29 14:28:22 +0100

    Class Person finished. Also updated the pom file and took away plug-ins.

--- src/main/java/io/github/patrikalm/Main.java
+++ src/main/java/io/github/patrikalm/Main.java
@@ -3,5 +3,11 @@ package io.github.patrikalm;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Person person1 = new Person("Mehrdad", "Javan", "Mehrdad@javan.com");
+
+        person1.getSummary();
+
     }
+
 }
--- src/main/java/io/github/patrikalm/Person.java
+++ src/main/java/io/github/patrikalm/Person.java
@@ -1,24 +1,38 @@
 package io.github.patrikalm;
 
+import java.util.Objects;
+
 public class Person {
 
+    private static int sequencer = 1000;
     private int id;
     private String firstName;   //have to fix check null not allowed
     private String lastName;    //have to fix check null not allowed
     private String email;
 
 
-
     //constructor
 
+    public Person(String firstName, String lastName, String email) {
+
+        this.id =getId();
+        setFirstName(firstName);
+        setLastName(lastName);
+        setEmail(email);
+
+    }
+
+    public Person(String firstName, String lastName){
+
+        new Person(firstName, lastName, "Email not available.");
+    }
 
     //methods
 
     public int getId() {
 
-        //have to make a sequencer
+        return sequencer++;
 
-        return id;
     }
 
     public String getFirstName() {
@@ -28,9 +42,45 @@ public class Person {
 
     public void setFirstName(String firstName) {
 
+        if (firstName == null || firstName.trim().isEmpty())
+            throw new IllegalArgumentException("Firstname can not be null or empty.");
+
         this.firstName = firstName;
     }
 
+    public String getLastName() {
 
+        return lastName;
+    }
 
+    public void setLastName(String lastName) {
+
+        if (lastName == null || lastName.trim().isEmpty())
+            throw new IllegalArgumentException("Lastname can not be null or empty.");
+
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+
+        return email;
+
+    }
+
+    public void setEmail(String email) {
+        if (email == null || email.trim().isEmpty())
+            throw new IllegalArgumentException("Email can not be null or empty.");
+        this.email = email;
+    }
+
+    public void getSummary() {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("ID number: " + id);
+        sb.append(", Name: " + firstName + " " + lastName);
+        sb.append(", Email: " + email);
+
+        System.out.println(sb);
+    }
 }
